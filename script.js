10b071f4d98dfc36e0d786894550af7b85a8cb75
biaxz <hajarboss@yahoo.com>
2023-06-27 19:55:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-$(document).ready(function() {
-  // Toggle navigation menu
-  $('.toggle').click(function() {
-    $('.list-item').toggleClass('active');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const navMenu = document.querySelector('.nav-menu');
+const navMenuItems = document.querySelectorAll('.nav-menu-item');
+
+hamburgerMenu.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
+
+navMenuItems.forEach((menuItem) => {
+  menuItem.addEventListener('click', () => {
+    navMenu.classList.remove('active');
   });
 });
